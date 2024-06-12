adbf4d0ce09238fce034cfd80ce1464030bf40d9
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2024-06-12 12:00:01 -0300

    fix(exG): conserta erro no calculo de altura

--- ABBAltura.c
+++ ABBAltura.c
@@ -10,17 +10,12 @@ typedef struct no {
 // Função que retorna a altura da árvore binária de busca
 int altura(no *r) {
     if (r == NULL) {
-        return -1; // Altura de uma árvore vazia é -1
+        return 0; // Altura de uma árvore vazia é -1
     } else {
         // Calcula a altura das subárvores esquerda e direita
         int altura_esq = altura(r->esq);
         int altura_dir = altura(r->dir);
         
-        // A altura da árvore é o máximo entre as alturas das subárvores mais 1
-        if (altura_esq > altura_dir) {
-            return altura_esq + 1;
-        } else {
-            return altura_dir + 1;
-        }
+        return (altura_esq > altura_dir ? altura_esq : altura_dir) + 1;
     }
 }
